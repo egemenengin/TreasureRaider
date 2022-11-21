ad3234421f6f8010ef4f1cb5765d7b2afebc1e15
egemenengin <58360345+egemenengin@users.noreply.github.com>
2022-11-21 22:51:33 +0300

    User Interface Beginning & File Hierarchy Arrangement

---
+++ Source/TreasureRaider/LoseHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LoseHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TREASURERAIDER_API ULoseHUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TreasureRaider/PauseHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PauseHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TREASURERAIDER_API UPauseHUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
